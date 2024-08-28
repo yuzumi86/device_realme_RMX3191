3e74108b6da2546e0b5f9b3c068ed021c2e59e56
yuzumi <157438148+yuzumi86@users.noreply.github.com>
2024-08-28 09:53:37 +0700

    ArrowOS thirteen

--- device.mk
+++ device.mk
@@ -27,6 +27,11 @@ PRODUCT_USE_DYNAMIC_PARTITIONS := true
 TARGET_SCREEN_HEIGHT := 1600
 TARGET_SCREEN_WIDTH := 720
 
+
+# Extra VNDK Versions
+PRODUCT_EXTRA_VNDK_VERSIONS := 30
+
+
 # Audio
 PRODUCT_PACKAGES += \
     android.hardware.audio@6.0.vendor \
@@ -34,15 +39,18 @@ PRODUCT_PACKAGES += \
     android.hardware.audio@6.0-impl:32 \
     android.hardware.audio.effect@6.0-impl:32 \
     android.hardware.audio.common@6.0-util \
+    android.hardware.audio.common-util.vendor \
+    android.hardware.audio.common@6.0-util.vendor \
     android.hardware.audio@6.0-util \
-    android.hardware.soundtrigger@2.3.vendor 
+    android.hardware.audio@6.0-util.vendor \
+    android.hardware.soundtrigger@2.3.vendor \
+    android.hardware.bluetooth.audio-impl \
 
 PRODUCT_PACKAGES += \
-    audio.a2dp.default \
+    audio_policy.stub \
     audio.bluetooth.default \
     audio.r_submix.default \
     audio.usb.default \
-    audio_policy.stub
 
 PRODUCT_PACKAGES += \
     libaudiofoundation.vendor \
@@ -50,9 +58,9 @@ PRODUCT_PACKAGES += \
     libbundlewrapper \
     libdownmix \
     libtinycompress \
-    libtinyxml \
     libalsautils \
     libnbaio_mono
+
 
 # Audio
 PRODUCT_COPY_FILES += \
@@ -80,9 +88,9 @@ PRODUCT_COPY_FILES += \
 
 # Bluetooth
 PRODUCT_PACKAGES += \
+    libbluetooth_audio_session \
     android.hardware.bluetooth.a2dp@1.0 \
     android.hardware.bluetooth@1.0.vendor \
-    android.hardware.bluetooth.audio@2.1-impl \
     android.hardware.bluetooth.a2dp@1.0.vendor
 
 PRODUCT_PACKAGES += \
